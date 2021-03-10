0f61869bf65797babce992f742b078daaff6f54d
Abd Abdullah <abdonlyone@gmail.com>
2021-03-10 12:34:49 +0600

    So

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useEffect, useState } from 'react';
 import './App.css';
+import League from './components/League/League';
 
 function App() {
+const [leagues, setLeagues] =useState({});
+
+  useEffect(()=> {
+    fetch('https://www.thesportsdb.com/api/v1/json/1/all_leagues.php')
+    .then(res => res.json())
+    .then(data => setLeagues(data))
+    console.log(leagues.leagues);
+  }, [])
   return (
     <div className="App">
-     
+      <h1>Leagues: {leagues.leagues.length}  </h1>
+     <League></League>
     </div>
   );
 }
